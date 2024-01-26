c9c66ce2cc8e175da95022d9533650da47b65de2
João Henrique <108812931+JoaoHenriquelm@users.noreply.github.com>
2024-01-26 02:04:47 -0300

    add assets projeto login

--- exercicios/ex026/mq005/script.js
+++ exercicios/ex026/mq005/script.js
@@ -1,16 +1,15 @@
 function clique() {
-    if (menu.style.display == 'block') {
-    menu.style.display = 'none'
-    } else {
-    menu.style.display = 'block'
-    }
+  if (menu.style.display == "block") {
+    menu.style.display = "none"
+  } else {
+    menu.style.display = "block"
+  }
 }
 
-
 function mudouTamanho() {
-    if (window.innerWidth >= 768) {
-        menu.style.display = 'block'
-    } else {
-        menu.style.display = 'none'
-    }
+  if (window.innerWidth >= 768) {
+    menu.style.display = "block"
+  } else {
+    menu.style.display = "none"
+  }
 }
